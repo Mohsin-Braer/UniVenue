dda54e373d28de824f2045be0559f9f63701c6df
Mohsin Braer <hbraer@gmail.com>
2023-08-18 01:35:52 -0400

    change to setup

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,5 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import request from 'supertest'
+import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../app';
 // import { sign } from 'jsonwebtoken';
